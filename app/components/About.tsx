fcd5f5e48c6e9a776957dc7eb678fd312697b632
Rafael Lopes <rafael.lopesdesigner@gmail.com>
2024-01-15 10:33:03 -0300

    update main

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -4,11 +4,7 @@ import Image from 'next/image';
 const About = () => {
   return (
     <div className="w-full grid grid-cols-2 items-center justify-between mt-10 mb-10">
-      <p className="">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae eius
-        unde assumenda quia eum voluptates nobis, odit in sed dolorem quasi
-        debitis nemo perferendis! Porro velit rem veritatis natus.
-      </p>
+      <p className=""></p>
       <Image
         src="/about.png"
         alt="Nosso Escritório"
